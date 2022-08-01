8c718168e4caa249768e3e6bee67e7d513f61f5b
Üsame Gül <usame.gll@gmail.com>
2022-08-01 16:59:06 +0300

    Update data_collecting.py

--- data_collecting.py
+++ data_collecting.py
@@ -6,4 +6,11 @@ def data_collect(interface):
 scapy.sniff(iface="eth0", store=False, prn=data_analysis)
 
 def data_analysis(packet):
-  packet.show()
+  #packet.show()
+  if packet.haslayer(http.HTTPRequest):
+    if packet.haslayer(scapy.Raw):
+       print(packet[scapy.Raw].load)
+    
+data_collect("eth0")
+
+  
